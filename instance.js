54d74afb297e272468138db652e51a53630a4114
strivenous <65793524+strivenous@users.noreply.github.com>
2020-06-01 03:14:45 +0200

    speedtest feature to fix

--- instance.js
+++ instance.js
@@ -2,6 +2,59 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var discordusertoken = 'NzE2NjYxNTA1MzA2Nzg3ODkw.XtPEXg.YP7zzVZNpm1N3646I27-x47bVSU'
 
+const NetworkSpeed = require('network-speed');
+const testNetworkSpeed = new NetworkSpeed();
+
+
+
+
+var netSpeed;
+netSpeed = {
+    dwl: {
+        value: ""
+    },
+
+    upl: {
+        value: ""
+    },
+
+    uplTarget: {
+        value: ""
+    }
+}
+getNetwDwlSpeed();
+
+netSpeed.uplTarget = 'www.google.com'; // html net upload speed target
+async function getNetwDwlSpeed() { // download speed
+
+    const baseURL = 'http://eu.httpbin.org/stream-bytes/50000000'
+    const fileSizeInBytes = 50000000
+    netSpeed.dwl = await testNetworkSpeed.checkDownloadSpeed(baseUrl, fileSizeInBytes);
+
+    
+}
+getNetUplSpeed();
+
+async function getNetUplSpeed() {
+
+    const options = {
+        hostname: netSpeed.uplTarget,
+        port: 80,
+        path: 'net-speed\50000000',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+
+        },
+
+    };
+
+    const fileSizeInBytes = 50000000
+    netSpeed.upl = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
+
+}
+
+
 const prefix = 'elitism.'
 client.on('ready', () => {
     console.log('ready.');
@@ -33,6 +86,18 @@ client.on('message', message => {
         if (message.content.includes('2d57799bb67f9d52f9f9063b0d6c9d03')) {
             message.channel.send('violenttaxinights');
         }
+
+    }
+});
+
+
+client.on('message', message => {
+    if (message.content === prefix + 'speedtest') {
+        message.channel.send('checking');
+        getNetUplSpeed();
+        message.channel.send(netSpeed.upl + ' value');
+        getNetwDwlSpeed();
+        message.channel.send(netSpeed.dwl + ' value');
 
     }
 });
